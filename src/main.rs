a41522cdc1c673cf707a91974d7c28003766191d
Ronan Keane <ronankeane269@gmail.com>
2024-06-23 12:14:46 +0200

    create separate library and binary crates

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,14 @@
 #[cfg(test)]
 mod tests;
 
-mod lib;
+use ryanair_api;
 
 #[tokio::main]
 async fn main() {
     let origin = "DUB";
     let destination = "STN";
 
-    match lib::get_one_way_flights(origin, destination, "2024-08-18").await {
+    match ryanair_api::get_one_way_flights(origin, destination, "2024-08-18").await {
         Ok(res) => {
             println!("Direct Flights: {}", res);
         },
@@ -17,7 +17,7 @@ async fn main() {
         }
     };
 
-    match lib::get_return_flights(origin, destination, "2024-08-18", "2024-04-19").await {
+    match ryanair_api::get_return_flights(origin, destination, "2024-08-18", "2024-04-19").await {
         Ok(res) => {
             println!("Return Flights: {}", res);
         },
@@ -26,7 +26,7 @@ async fn main() {
         }
     };
 
-    match lib::get_cheapest_return_flights_from_weekdays(origin,destination,"2024-08-17", "2024-09-17", "monday", "sunday").await {
+    match ryanair_api::get_cheapest_return_flights_from_weekdays(origin,destination,"2024-08-17", "2024-09-17", "monday", "sunday").await {
         Ok(res) => {
             println!("All Possible Flights:\n{}", res);
         },
